dea5de5906b0a42a135c4f9e21ce97cafd742c17
EgyAbdo10 <egyabdo490@gmail.com>
2024-01-04 14:11:57 +0200

    added or modified a file

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -6,7 +6,8 @@
  * @second: the second node
  * Return: the lowest ansector
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+const binary_tree_t *second)
 {
 binary_tree_t *second_iter;
 while (first != NULL)
