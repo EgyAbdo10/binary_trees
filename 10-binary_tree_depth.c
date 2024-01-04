9737f1d57157071c6a750acf41dea8a2a50ebf55
EgyAbdo10 <egyabdo490@gmail.com>
2024-01-04 08:12:57 +0200

    added or modified a file

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -6,8 +6,9 @@
 */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+size_t depth;
 if (tree->parent == NULL)
 return (0);
-size_t depth = binary_tree_depth(tree->parent);
+depth = binary_tree_depth(tree->parent);
 return (++depth);
 }
