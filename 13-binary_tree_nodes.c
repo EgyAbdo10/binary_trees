047067c343d142e8caef521f366b8cba00fe7146
EgyAbdo10 <egyabdo490@gmail.com>
2024-01-04 09:58:21 +0200

    added or modified a file

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -10,11 +10,9 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 size_t left_count, right_count;
 if (tree == NULL)
 return (0);
-if (tree->left != NULL || tree->right != NULL)
-return (1);
-else
-return (0);
 left_count = binary_tree_nodes(tree->left);
+if (tree->left != NULL || tree->right != NULL)
+left_count++;
 right_count = binary_tree_nodes(tree->right);
 return (left_count + right_count);
 }
