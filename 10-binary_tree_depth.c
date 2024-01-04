b9c59c10e67e81a6faf7f8c5e7a2395527915898
EgyAbdo10 <egyabdo490@gmail.com>
2024-01-04 04:16:44 +0200

    get depth

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - get the depth of a node
+ * @tree: a pointer to the tree
+ * Return: the depth
+*/
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+if (tree->parent == NULL)
+return (0);
+size_t depth = bi_tree_get_depth(tree->parent);
+return (depth++);
+}
